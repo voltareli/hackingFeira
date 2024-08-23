a58bdb33383d168ffb4f2ea761b4bd03e5179253
beferreira <ra43020907829@acaonsfatima.org.br>
2024-08-23 17:38:10 -0300

    base pronta

--- src/pages/login/App.jsx
+++ src/pages/login/App.jsx
@@ -4,9 +4,49 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
- 
-      <h1>oi</h1>
+      <div className='login'>
+        <div className='imagens'>
 
+        </div>
+        <div className='interativo'>
+          <div className='quadradoUm'>
+            <img className='instagramEscrita' src="" alt="" />
+            <div className='campos'>
+
+            </div>
+
+          </div>
+          <div className='quadradoDois'>
+            <p>Não tem uma conta?</p><a href="#">Cadastre-se</a>
+          </div>
+          <p>Obtenha o aplicativo.</p>
+          <div className='baixar'>
+            <button><img src="" alt="" /></button>
+            <button><img src="" alt="" /></button>
+          </div>
+        </div>
+      </div>
+      <div className='rodape'>
+        <nav>
+          <a href="">Meta</a>
+          <a href="">Sobre</a>
+          <a href="">Blog</a>
+          <a href="">Carreiras</a>
+          <a href="">Ajuda</a>
+          <a href="">API</a>
+          <a href="">Privacidade</a>
+          <a href="">Termos</a>
+          <a href="">Localizações</a>
+          <a href="">Instagram Lite</a>
+          <a href="">Threads</a>
+          <a href="">Carregamento de contatos e não usuários</a>
+          <a href="">Meta Verified</a>
+        </nav>
+        <div className='final'>
+          <p>Português (Brasil) e313</p>
+          <p>© 2024 Instagram from Meta</p>
+          </div>
+      </div>
     </div>
   );
 }
