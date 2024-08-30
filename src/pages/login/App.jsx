2d44da0346f0ad7b37a815638f15e89d41c95305
voltareli <nivoltareli@gmail.com>
2024-08-30 16:38:40 -0300

    atualizado

--- src/pages/login/App.jsx
+++ src/pages/login/App.jsx
@@ -20,6 +20,9 @@ const[Senha, setSenha]= useState(0)
     <div className="App">
         <div className='interativo'>
 
+
+          <div className='quadrados'>
+
           <div className='quadradoUm'>
 
             <img className='instagramEscrita' src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png" alt="" />
@@ -58,9 +61,10 @@ const[Senha, setSenha]= useState(0)
             <button className='play'><img src="https://static.cdninstagram.com/rsrc.php/v3/y_/r/tUzYKZ-xrQK.png" alt="" /></button>
             <button className='micro'><img src="https://static.cdninstagram.com/rsrc.php/v3/yE/r/QQnPXT5YsC4.png" alt="" /></button>
           </div>
+           </div>
           </div>
-      </div>
-      <footer>
+
+      
           <div className='rodape'>
           <nav>
             <a href="">Meta</a>
@@ -76,13 +80,22 @@ const[Senha, setSenha]= useState(0)
             <a href="">Threads</a>
             <a href="">Carregamento de contatos e não usuários</a>
             <a href="">Meta Verified</a>
+
+      
           </nav>
-          <div className='final'>
-            <p>Português (Brasil)</p>
-            <p>© 2024 Instagram from Meta</p>
-          </div>
+          
+          <div className='linha2'>
+
+<a href="">Português (Brasil)</a>
+<a href="">© 2024 Instagram from Meta</a>
+</div>
+        
         </div>
-      </footer>
+
+      </div>
+     
+         
+
     </div>
   );
 }
